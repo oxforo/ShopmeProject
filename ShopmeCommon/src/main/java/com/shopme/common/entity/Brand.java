1a16123e4726dc8ed6dde2e1ea0b03efb7adecf7
oxforo <oxforo@naver.com>
2023-01-19 23:56:17 +0900

    [Feat] 브랜드 기능 * Brand 엔티티 클래스 생성 * Brand 레포지토리 클래스 생성

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -0,0 +1,37 @@
+package com.shopme.common.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 45, unique = true)
+    private String name;
+
+    @Column(nullable = false, length = 128)
+    private String logo;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "brands_categories",
+            joinColumns = @JoinColumn(name = "brand_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+
+    public Brand(String name) {
+        this.name = name;
+        this.logo = "brand-logo.png";
+    }
+}
