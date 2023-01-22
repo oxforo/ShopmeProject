2500d5a57a7934a386949bdfa21850025e840996
oxforo <oxforo@naver.com>
2023-01-22 17:51:14 +0900

    [Refact] 브랜드 기능 * 브랜드 샘플 데이터 업로드 * URL 기반 파일 추출 기능 내 중복 코드 제거

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -13,29 +13,18 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        String dirName = "user-photos";
-        Path userPhotosDir = Paths.get(dirName);
-
-        String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/" + dirName + "/**")
-                .addResourceLocations("file:" + userPhotosPath + "/");
-
-        String categoryImageName = "../categories-images";
-        Path categoryImagesDir = Paths.get(categoryImageName);
-
-        String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/categories-images/**")
-                .addResourceLocations("file:" + categoryImagesPath + "/");
-
-        String brandImageName = "../brands-logos";
-        Path brandImagesDir = Paths.get(brandImageName);
-
-        String brandImagesPath = brandImagesDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/brands-logos/**")
-                .addResourceLocations("file:" + brandImagesPath + "/");
+        exposeDirectory("user-photos", registry);
+        exposeDirectory("../categories-images", registry);
+        exposeDirectory("../brands-logos", registry);
     }
 
+    private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+        Path path = Paths.get(pathPattern);
+        String absolutePath = path.toFile().getAbsolutePath();
+
+        String logicalPath = pathPattern.replace("../", "") + "/**";
+
+        registry.addResourceHandler(logicalPath)
+                .addResourceLocations("file:" + absolutePath + "/");
+    }
 }
