ff48e4835e292d3df4e2eaf21abf3eb32bac4efe
oxforo <oxforo@naver.com>
2023-02-14 22:52:11 +0900

    [Feat] Customer Customer 엔티티 및 레포지토리 추가

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Customer.java
@@ -0,0 +1,65 @@
+package com.shopme.common.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @Column(name = "phone_number", nullable = false, length = 15)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 64)
+    private String addressLine1;
+
+    @Column(name = "address_line_2", length = 64)
+    private String addressLine2;
+
+    @Column(nullable = false, length = 45)
+    private String city;
+
+    @Column(nullable = false, length = 45)
+    private String state;
+
+    @Column(name = "postal_code", nullable = false, length = 10)
+    private String postalCode;
+
+    @Column(name = "verification_code", length = 64)
+    private String verificationCode;
+
+    private boolean enabled;
+
+    @Column(name = "crated_time")
+    private Date createdTime;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    public Customer() {
+    }
+}
